e46860419433d585061dd09ff8fc4447c283a316
Mayank-god <mrawat24680@gmail.com>
2025-10-05 23:41:16 +0530

    SumOfDigitUptoNTerms

---
+++ SumUpto_N_Terms.c
@@ -0,0 +1,22 @@
+//WAP to print the sum of all numbers upto a given number n.
+
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    int sum=0;
+
+    printf("Enter upto which number you want sum of: ");
+    scanf("%d",&n);
+
+    //Loop to sum all the digits
+    for (int i = 1 ; i<=n ; i++){
+        sum = sum + i;
+    }
+
+    printf("The sum of all digits upto %d : %d", n , sum);
+
+    return 0;
+
+}
