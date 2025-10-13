c2ec7d750ff6562203bc63a1682ac8c9975b55d5
Mayank-god <mrawat24680@gmail.com>
2025-10-13 22:37:39 +0530

    PatternPrinting6.c

--- PatternPrinting6.c
+++ PatternPrinting6.c
@@ -8,17 +8,17 @@
 int main(){
 
     int row;
+
     printf("Enter the value of row :");
     scanf("%d",&row);
 
-    for ( int i=1 ;i <= 2*row - 1;i += 2){
+    for ( int i = 1 ;i <= 2*row - 1;i += 2){
         for ( int j= 1 ; j<=i ; j+=2){
         printf("%d",j);
         printf(" ");
        }
+
        printf("\n");
     }
-
-
     return 0;
 }
