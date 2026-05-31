1a1e998215a5c9741d58b617db80b11c57690b6a
Mayank-god <mrawat24680@gmail.com>
2026-05-31 21:04:49 +0530

    Update TablePrinting.c

--- TablePrinting.c
+++ TablePrinting.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-//WAP AND MAKE A FUNCTION WHICH TAKE A NUMBER AND PRINT IT'S TABLE.
-
-void printable(int n);
+//WAP TO TAKE A NUMBER AND PRINT IT'S TABLE.
 
 int main(){
 
@@ -11,15 +9,8 @@ int main(){
     printf("Enter number that you want to have table of :\t");
     scanf("%d",&num);
 
-    printable(num);
-    return 0;
-
-    //FUNCTION
-}
-
-void printable(int n){
     for(int b=1 ; b<=10; b++){
-        printf("%d * %d = %d \n",n , b , b*n);
+        printf("%d * %d = %d \n",num , b , b*num);
     }
-
+    return 0;
 }
